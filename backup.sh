ded6e3ea2c5c9622ac504b37c6e94649e0987cbf
Adriano Pezzuto <kalise@users.noreply.github.com>
2017-11-11 19:32:56 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Copyright 2017 - Adriano Pezzuto
+# https://github.com/kalise
+#
+# Database credentials
+user="root"
+password=""
+host="localhost"
+server=$(hostname)
+db_name="employees"
+# other options
+backup_path="/etc/mysql/backup/data"
+bucket_name="mysql-backups-storage"
+date=$(date +"%H:%M:%S-%d-%b-%Y")
+# Set default file permissions
+umask 177
+# Dump database into SQL file
+mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$server-$db_name-$date.sql
+# Delete files older than 2 days from local machine
+find $backup_path/* -mtime +30 -exec rm {} \;
+# Synchronize local backup directory to a Google Cloud Storage bucket
+gsutil rsync -r $backup_path gs://$bucket_name
