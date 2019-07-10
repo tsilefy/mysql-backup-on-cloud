b9ece4db6aef6bc57cfb3691b47a759a6d54620e
tsilefy <tsilefy+github@gmail.com>
2019-07-10 20:48:51 +0200

    gzip the sql dump and delete files older than 14 days

--- backup.sh
+++ backup.sh
@@ -15,8 +15,8 @@ date=$(date +"%H:%M:%S-%d-%b-%Y")
 # Set default file permissions
 umask 177
 # Dump database into SQL file
-mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$server-$db_name-$date.sql
-# Delete files older than 2 days from local machine
-find $backup_path/* -mtime +30 -exec rm {} \;
+mysqldump --user=$user --password=$password --host=$host $db_name | gzip -c > $backup_path/$server-$db_name-$date.sql.gz
+# Delete files older than 14 days from local machine
+find $backup_path/* -mtime +14 -exec rm {} \;
 # Synchronize local backup directory to a Google Cloud Storage bucket
 gsutil rsync -r $backup_path gs://$bucket_name
